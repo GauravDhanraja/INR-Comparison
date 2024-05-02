205d0ea86267719682060fbe2af86fb7dde35e8c
GauravDhanraja <gauravdhanraja88@proton.me>
2024-05-03 00:36:22 +0530

    Reoved the value check in data_from_url_to_csv

--- currencydata.py
+++ currencydata.py
@@ -45,16 +45,14 @@ def get_from_csv(file: str) -> list:
 
 def data_from_url_to_csv(file: str) -> None:
     url = 'https://www.x-rates.com/table/?from=INR&amount=1'
-    response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     table = soup.find('table', {'class': 'tablesorter ratesTable'})
     rows = table.find_all('tr')
 
-    # Extract data from table rows
     data = [row_data[:3] for row_data in [[str.strip(tag.text) for tag in row.find_all('td')] for row in rows
-          if row.find_all('td') and len(row.find_all('td')) >= 3]]
+                                          if row.find_all('td') and len(row.find_all('td')) >= 3]]
 
-    data = [[country, float(inr), float(inverse_inr)] for country, inr, inverse_inr in data if float(inr) <= 150]
+    data = [[country, float(inr), float(inverse_inr)] for country, inr, inverse_inr in data]
 
     save_to_csv(data, file)
 
