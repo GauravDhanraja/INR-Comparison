09a64011bb1729504f08c7833c621c7b57ff1dea
GauravDhanraja <gauravdhanraja88@proton.me>
2024-05-03 21:33:18 +0530

    tweaked nudege value in sml_plot

--- plot.r
+++ plot.r
@@ -19,12 +19,12 @@ sml_plot <- ggplot(data, aes(x=INR, y=inv_INR)) +
             geom_point() +
             geom_text(
                 label=rownames(data), 
-                nudge_x = 0.01, nudge_y = 0.1, 
-                check_overlap = T
+                nudge_x = 0.005, nudge_y = 0.1, 
+                check_overlap = F
             ) +
             scale_x_continuous(limits = c(0, 0.7)) +
             scale_y_continuous(limits = c(0, 25))
 
 ggsave("./plots/lrgplot.png", plot = lrg_plot, width = 20, height = 14, dpi = 1000)
-ggsave("./plots/smlplot.png", plot = sml_plot, width = 20, height = 20, dpi = 1000)
+ggsave("./plots/smlplot.png", plot = sml_plot, width = 26, height = 20, dpi = 1000)
 
